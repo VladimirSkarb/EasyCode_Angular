7d6205a55101d63960792f4bf2b7852029266017
soulr1ver <dovgal_maxim@mail.ru>
2019-12-14 01:00:24 +0200

    Added spaces and semicolons.

--- HW1/app.ts
+++ HW1/app.ts
@@ -1,19 +1,19 @@
 //2)реализовать метод который принимает число натуральное(int >0)и выдает массив чисел фибоначи до заданного числа
 // fn(2) => [1,1]
 // fn(4) => [1,1,2,3]
-function fn(num:number): Array<number> {
-    let arr:Array<number> = [1];
+function fn(num: number): Array<number> {
+    let arr: Array<number> = [1];
     for (let i:number = 1; i <= num; i = arr[arr.length - 1] + arr[arr.length - 2]) {
-        arr.push(i)
+        arr.push(i);
     }
-    return arr
+    return arr;
 }
 
 console.log(fn(60));
 
 
 //1)реализовать метод принимающий строку и возвращающий является ли она полиндромом
-function isPalindrome(str:string):boolean {
+function isPalindrome(str: string): boolean {
     let strReverse:string = str.split('').reverse().join('');
     return strReverse === str;
 }
