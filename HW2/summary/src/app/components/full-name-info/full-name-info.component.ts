4aaa9f8804b567b1aa91876a09479a4282a26eac
VladimirSkarb <vovan63@gmail.com>
2019-12-22 23:07:51 +0200

    Created HW2

---
+++ HW2/summary/src/app/components/full-name-info/full-name-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-name-info',
+  templateUrl: './full-name-info.component.html',
+  styleUrls: ['./full-name-info.component.css']
+})
+export class FullNameInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
